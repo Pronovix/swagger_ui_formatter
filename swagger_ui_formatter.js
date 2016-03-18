02de6e811ad5a8d6a232975a8160c72fbcafc61b
Sudheesh Damodaran <dsudheesh@gmail.com>
2016-03-18 18:41:31 +0530

    Added field formatter settings for the swagger ui options

--- swagger_ui_formatter.js
+++ swagger_ui_formatter.js
@@ -3,16 +3,27 @@
  * Swagger UI custom javascript.
  */
 
+window.SwaggerUiList = [];
     (function($) {
       Drupal.behaviors.swagger_ui_formatter = {
         attach: function(context, settings) {
           var swagger_files = Drupal.settings.swagger_ui_formatter.swagger_files;
+          var docExpansion = Drupal.settings.swagger_ui_formatter.doc_expansion;
+          var validatorUrl = Drupal.settings.swagger_ui_formatter.validator_url;
+          var showRequestHeaders = Drupal.settings.swagger_ui_formatter.show_request_headers;
+          var options = {
+            docExpansion: docExpansion,
+            showRequestHeaders: showRequestHeaders
+          };
+          if (validatorUrl !== false) {
+            options.validatorUrl = validatorUrl;
+          }
           var swagger_ui = [];
           for (var i = 0; i < swagger_files.length; i++) {
-            swagger_ui[i] = new SwaggerUi({
-              url: swagger_files[i].url,
-              dom_id: "swagger-ui-container-" + i
-            });
+            options.url = swagger_files[i].url;
+            options.dom_id = "swagger-ui-container-" + i;
+            swagger_ui[i] = new SwaggerUi(options);
+            window.SwaggerUiList[i] = swagger_ui[i];
           }
           for (var i = 0; i < swagger_ui.length; i++) {
             swagger_ui[i].load();
