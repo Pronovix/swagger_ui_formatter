0234170d3e4c8976102fd24911c88746d5bcea1b
Balazs Wittmann <balazs.wittmann@pronovix.com>
2019-04-30 14:33:09 +0200

    Issue #3051703 by balazswmann: Add missing core/jquery.once dependency to swagger_ui_integration library definition

---
+++ swagger_ui_formatter.post_update.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Post update functions for Swagger UI Field Formatter.
+ */
+
+/**
+ * Clear cache due to updated library definitions.
+ */
+function swagger_ui_formatter_post_update_1() {
+  // Empty post-update hook.
+}
