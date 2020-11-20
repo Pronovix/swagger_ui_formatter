e446ed34157cc1a90fd656b729a065b995c7e748
Balazs Wittmann <balazs.wittmann@pronovix.com>
2020-11-20 14:43:13 +0100

    Fix post update hook related cache flush

--- swagger_ui_formatter.post_update.php
+++ swagger_ui_formatter.post_update.php
@@ -8,21 +8,21 @@
 declare(strict_types = 1);
 
 /**
- * Clear cache due to updated library definitions.
+ * Rebuild cache due to updated library definitions.
  */
 function swagger_ui_formatter_post_update_1(): void {
   // Empty post-update hook.
 }
 
 /**
- * Clear cache due to changes in the rendering.
+ * Rebuild cache due to changes in the rendering.
  */
 function swagger_ui_formatter_post_update_2(): void {
   // Empty post-update hook.
 }
 
 /**
- * Clear cache due to implementation changes.
+ * Rebuild cache due to implementation changes.
  */
 function swagger_ui_formatter_post_update_3(): void {
   // Empty post-update hook.
