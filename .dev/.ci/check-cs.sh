27ed0ced74c2e8839f718f430d09045cfa5cb124
Dezső BICZÓ <mxr576@users.noreply.github.com>
2021-08-13 14:09:56 +0200

    Add GitHub Actions to the project (#62) (#72)
    Co-authored-by: lekob <34817163+lekob@users.noreply.github.com>

--- .dev/.ci/check-cs.sh
+++ .dev/.ci/check-cs.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ ${CHECK_CS} == true ]]; then
-    docker-compose exec php composer normalize -d .. --indent-size=4 --indent-style=space --no-update-lock --dry-run
-    docker-compose exec php ./vendor/bin/phpcs -s ../phpcs.xml.dist --ignore="contrib/*" web/modules
-    docker-compose exec php ./vendor/bin/drupal-check --drupal-root=. -e "*/build/*" ..
+    docker-compose exec -T php composer normalize -d .. --indent-size=4 --indent-style=space --no-update-lock --dry-run
+    docker-compose exec -T php ./vendor/bin/phpcs -s ../phpcs.xml.dist --ignore="contrib/*" web/modules
+    docker-compose exec -T php ./vendor/bin/drupal-check --drupal-root=. -e "*/build/*" ..
 fi
