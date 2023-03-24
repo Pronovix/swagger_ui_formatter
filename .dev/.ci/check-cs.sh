e2867c0af002500fe0daa8ae3a08842319112bb5
Dezső BICZÓ <dezso.biczo@pronovix.com>
2023-03-24 12:55:58 +0100

    Drop PHP 8.0 support

--- .dev/.ci/check-cs.sh
+++ .dev/.ci/check-cs.sh
@@ -4,8 +4,6 @@ set -e
 
 if [[ ${CHECK_CS} == true ]]; then
     docker-compose exec -T php composer normalize -d .. --no-update-lock --dry-run
-    # Code style must comply with our minimum required PHP version.
-    docker-compose exec -T php ./vendor/bin/phpcs --config-set php_version 80000
     # @TODO There is a weird file access issue on GHA.
     # Drupal QA: Unable to set 511 visibility for file at /mnt/files/local_mount.
     docker-compose exec -T php ./vendor/bin/phpcs -s ./vendor/pronovix/drupal-qa/config/phpcs.xml.dist --ignore="contrib/*,*/build/*" web/modules
