8b8ee6b9b83860b5daad7b25df9ab4003cb6e083
Dezső BICZÓ <dezso.biczo@pronovix.com>
2024-04-08 14:20:38 +0200

    Use `docker compose` instead of `docker-compose`

--- .dev/.ci/check-cs.sh
+++ .dev/.ci/check-cs.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [[ ${CHECK_CS} == true ]]; then
-    docker-compose exec -T php composer normalize -d .. --no-update-lock --dry-run
+    docker compose exec -T php composer normalize -d .. --no-update-lock --dry-run
     # @TODO There is a weird file access issue on GHA.
     # Drupal QA: Unable to set 511 visibility for file at /mnt/files/local_mount.
-    docker-compose exec -T php ./vendor/bin/phpcs -s ./vendor/pronovix/drupal-qa/config/phpcs.xml.dist --ignore="contrib/*,*/build/*" web/modules
-    docker-compose exec -T php ./vendor/bin/phpstan --no-progress
+    docker compose exec -T php ./vendor/bin/phpcs -s ./vendor/pronovix/drupal-qa/config/phpcs.xml.dist --ignore="contrib/*,*/build/*" web/modules
+    docker compose exec -T php ./vendor/bin/phpstan --no-progress
 fi
--- .dev/.ci/run-phpunit-tests.sh
+++ .dev/.ci/run-phpunit-tests.sh
@@ -5,5 +5,5 @@ set -e
 if [[ ${RUN_PHPUNIT_TESTS} == true ]]; then
     # Custom bootstrap file is required to prevent infinite loop caused by symlinking that Drupal's original
     # bootstrap file cannot handle.
-    docker-compose run --rm php ./vendor/bin/phpunit -c web/core -v --debug --printer '\Drupal\Tests\Listeners\HtmlOutputPrinter' --bootstrap=/mnt/files/local_mount/tests/src/bootstrap.php web/modules/drupal_module/tests/
+    docker compose run --rm php ./vendor/bin/phpunit -c web/core -v --debug --printer '\Drupal\Tests\Listeners\HtmlOutputPrinter' --bootstrap=/mnt/files/local_mount/tests/src/bootstrap.php web/modules/drupal_module/tests/
 fi
